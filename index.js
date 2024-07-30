7cdecde29b369c1702278fb935c1ac87cbbd43a6
Mbasa <mbasacokile7@gmail.com>
2024-07-30 22:44:35 +0200

    Ensure proper display of headers and footer. Also basic CSS styling

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ import bodyParser from "body-parser";
 const app = express();
 // Allow Express to use the body-parser middleware
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 //-------------------------------HTTP Requests and Routing-------------------------------//
+
+// GET Request to render the landing page
+app.get("/", (req, res) =>{
+    res.render("index.ejs");
+})
 
 
 const port = 3000;
 //Initialise the Server and ensure its listening on Port 3000
 app.listen(port, (req, res) =>{
     console.log(`The server is running on Port: ${port}`);
-})
+});
