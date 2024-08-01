3e6f8417af83cc68e5bdbc5ad925186bedf27430
Mbasa <mbasacokile7@gmail.com>
2024-08-01 17:26:41 +0200

    Add routing using Express to all pages

--- index.js
+++ index.js
@@ -7,13 +7,41 @@ const app = express();
 // Allow Express to use the body-parser middleware
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+// Define a variable to store variable to store 
+let d = new Date();
+let todayDate = d.getMonth() + " " +  d.getDate() + " " + d.getFullYear();
 
 //-------------------------------HTTP Requests and Routing-------------------------------//
 
 // GET Request to render the landing page
 app.get("/", (req, res) =>{
+    res.render("index.ejs");
+});
+
+//Get request to route to the Create Page
+app.get("/create", (req, res) =>{
     res.render("create.ejs");
-})
+
+});
+
+//GET Request to get the Articles Page
+app.get("/article", (req, res) =>{
+    res.render("article.ejs");
+});
+
+//GET Requst to render the Read Page
+app.get("/read", (req, res)=>{
+    res.render("read.ejs");
+});
+
+let data = {};
+
+// POST REQUEST to submit user data into the Articles Page(from Create Page) and also from Articles page to Read Page
+app.post("/create", (req, res)=>{
+    data = req.body;
+    res.render("article.ejs", data);
+    
+});
 
 
 const port = 3000;
